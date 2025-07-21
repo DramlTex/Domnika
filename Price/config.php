71933e5436c4390241850191a13a54f9abdabbb5
DramlTex <63540011+DramlTex@users.noreply.github.com>
2025-07-21 14:44:44 +0300

    Update config.php

--- Price/config.php
+++ Price/config.php
@@ -1,6 +1,5 @@
 <?php
 return [
-  'login' => 'domnika.n@jfk.in',
-  'password' => 'Zima9091',
-  // Можно добавить и другие настройки, например, базу данных, API-токены и т.д.
+  'login' => 'tehcenter@jfk',
+  'password' => 'Asdf123456!',
 ];
